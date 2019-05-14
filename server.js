85667a528d7f0665e19e00cf01cb8249c6b4b876
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-05-14 15:25:04 +0900

    Handling signals properly

--- server.js
+++ server.js
@@ -5,3 +5,18 @@ app.get('/', (req, res) => { res.send('Hello World!'); });
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Web server listening on PORT ${server.address().port}`);
 });
+
+[
+  { name: 'SIGHUP', value: 1 },
+  { name: 'SIGINT', value: 2 },
+  { name: 'SIGTERM', value: 15 }
+].forEach(signal => {
+  process.on(signal.name, () => {
+    console.log(`Received signal ${signal.name}, shutting down`);
+    server.close(() => {
+      console.log(`web server shutdown`)
+      process.exit(128 + signal.value)
+    })
+  })
+})
+
